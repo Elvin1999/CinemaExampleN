7e8749c1003ec6ff3831f9c8a4946565752d188d
camalzadeelvin <camalzadeelvin@STHQ0118-04>
2022-11-25 15:57:24 +0400

    Add project files.

---
+++ CinemaExampleN/ViewModels/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using CinemaExampleN.Commands;
+using CinemaExampleN.Services;
+using CinemaExampleN.Views;
+using CinemaExampleN.Views.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CinemaExampleN.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+		private string searchText;
+
+		public string SearchText
+		{
+			get { return searchText; }
+			set { searchText = value; OnPropertyChanged(); }
+		}
+
+		private string backImagePath;
+
+		public string BackImagePath
+        {
+			get { return backImagePath; }
+			set { backImagePath = value; OnPropertyChanged(); }
+		}
+
+
+		public RelayCommand SearchCommand { get; set; }
+		public MainViewModel()
+		{
+			BackImagePath = @"\images\cine.jpg";
+			SearchCommand = new RelayCommand((p) =>
+			{
+				var panel = p as WrapPanel;
+				panel.Children.Clear();
+				var movies = MovieService.GetMovies(SearchText);
+				int x = 10;
+				int y = 10;
+
+				BackImagePath = movies.First().ImagePath;
+
+				foreach (var m in movies)
+				{
+					var ucVM = new MovieCellViewModel
+					{
+						Movie = m
+					};
+
+					var d = double.Parse(m.Rating.Replace('.', ',')).ToString();
+
+					double result = (double.Parse(d) + 1) / 2;
+					int count = (int)result;
+					var uc = new MovieCellUC(ucVM);
+
+					for (int i = 0; i < count; i++)
+					{
+						ucVM.StarsPanel.Children.Add(new StarUC());
+					}
+
+					uc.Margin = new System.Windows.Thickness(x, y, 0, 0);
+
+
+					panel.Children.Add(uc);
+				}
+			});
+		}
+	}
+}
---
+++ CinemaExampleN/ViewModels/MovieCellViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CinemaExampleN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CinemaExampleN.ViewModels
+{
+    public class MovieCellViewModel:BaseViewModel
+    {
+		private Movie movie;
+
+		public Movie Movie
+		{
+			get { return movie; }
+			set { movie = value;
+				movie.Description = movie.Description.Substring(0, 17);
+				if (movie.Name.Length >= 18)
+				{
+					movie.Name = movie.Name.Substring(18);
+				}
+				
+				OnPropertyChanged();			
+			}
+		}
+		public WrapPanel StarsPanel { get; set; }
+
+		public MovieCellViewModel()
+		{
+
+		}
+
+	}
+}
